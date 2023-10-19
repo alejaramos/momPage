cd17637e793c5d211a711f12c9a5b2cb6d929107
alejaRamosDev <alejandra.ramos@irixapp.com>
2023-10-19 13:31:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Carousel from 'react-bootstrap/Carousel'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() { 
+return ( 
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+	<div style={{ display: 'block', width: 580, padding: 50 }}> 
+	<Carousel fade> 
+		<Carousel.Item interval={4000}> 
+		<img 
+			className="d-block w-100 h-100"
+src="image2.png"
+			alt="Image One"
+		/> 
+		<Carousel.Caption>
+		</Carousel.Caption> 
+		</Carousel.Item> 
+		<Carousel.Item interval={3000}> 
+		<img 
+			className="d-block w-100 h-100"
+src="image1.png"
+			alt="Image Two"
+		/> 
+		<Carousel.Caption> 
+		</Carousel.Caption> 
+		</Carousel.Item> 
+    <Carousel.Item interval={3000}> 
+		<img 
+			className="d-block w-100 h-100"
+src="image3.png"
+			alt="Image One"
+		/> 
+		<Carousel.Caption> 
+		</Carousel.Caption> 
+		</Carousel.Item> 
+		<Carousel.Item interval={3000}> 
+		<img 
+			className="d-block w-100 h-100"
+src="image4.png"
+			alt="Image Two"
+		/> 
+		<Carousel.Caption> 
+		</Carousel.Caption> 
+		</Carousel.Item> 
+    <Carousel.Item interval={3000}> 
+		<img 
+			className="d-block w-100 h-100"
+src="image5.png"
+			alt="Image One"
+		/> 
+		<Carousel.Caption> 
+		</Carousel.Caption> 
+		</Carousel.Item> 
+		<Carousel.Item interval={3000}> 
+		<img 
+			className="d-block w-100 h-100"
+src="image6.png"
+			alt="Image Two"
+		/> 
+		<Carousel.Caption> 
+		</Carousel.Caption> 
+		</Carousel.Item> 
+	</Carousel> 
+	</div> 
+  	</div> 
+); 
 }
-
-export default App;
